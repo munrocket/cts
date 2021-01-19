fdb1f177028d5c6c12598041d8092653e28f6b65
kainino0x <kainino1@gmail.com>
2021-01-19 22:11:43 +0000

    Deploying to gh-pages from  @ 596c793a61f5b8b4cf489627b9f7781bd4a37a91 🚀

--- out-wpt/common/framework/version.js
+++ out-wpt/common/framework/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '34353d442827ab1a21072290476689ce90604cdd';
+export const version = '596c793a61f5b8b4cf489627b9f7781bd4a37a91';
--- out-wpt/webgpu/api/validation/resource_usages/texture/in_pass_encoder.spec.js
+++ out-wpt/webgpu/api/validation/resource_usages/texture/in_pass_encoder.spec.js
@@ -919,10 +919,10 @@ g.test('unused_bindings_in_pipeline')
     // TODO: revisit the shader code once 'image' can be supported in wgsl.
     const wgslFragment = pp`
       ${pp._if(useBindGroup0)}
-      [[set(0), binding(0)]] var<image> image0 : [[access(read)]] texture_storage_2d<rgba8unorm>;
+      [[group(0), binding(0)]] var<image> image0 : [[access(read)]] texture_storage_2d<rgba8unorm>;
       ${pp._endif}
       ${pp._if(useBindGroup1)}
-      [[set(1), binding(0)]] var<image> image1 : [[access(read)]] texture_storage_2d<rgba8unorm>;
+      [[group(1), binding(0)]] var<image> image1 : [[access(read)]] texture_storage_2d<rgba8unorm>;
       ${pp._endif}
       [[stage(fragment)]] fn main() -> void {}
     `;
@@ -930,10 +930,10 @@ g.test('unused_bindings_in_pipeline')
     // TODO: revisit the shader code once 'image' can be supported in wgsl.
     const wgslCompute = pp`
       ${pp._if(useBindGroup0)}
-      [[set(0), binding(0)]] var<image> image0 : [[access(read)]] texture_storage_2d<rgba8unorm>;
+      [[group(0), binding(0)]] var<image> image0 : [[access(read)]] texture_storage_2d<rgba8unorm>;
       ${pp._endif}
       ${pp._if(useBindGroup1)}
-      [[set(1), binding(0)]] var<image> image1 : [[access(read)]] texture_storage_2d<rgba8unorm>;
+      [[group(1), binding(0)]] var<image> image1 : [[access(read)]] texture_storage_2d<rgba8unorm>;
       ${pp._endif}
       [[stage(compute)]] fn main() -> void {}
     `;
