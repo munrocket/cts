0bc21c8c3cbcf0770a1c21e04fbd672e21b4190b
Kangz <corentin@wallez.net>
2021-02-08 17:07:48 +0000

    Deploying to gh-pages from  @ 3b86d142c341f06a1cbab468b2cfb8100430bd92 🚀

--- out/common/framework/version.js
+++ out/common/framework/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'b7cc50f593b8242bdcfc1737695d4577e8327ae0';
+export const version = '3b86d142c341f06a1cbab468b2cfb8100430bd92';
