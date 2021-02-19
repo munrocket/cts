2ee84032cde047fafd8201826377adac0c79f82f
kainino0x <kainino1@gmail.com>
2021-02-19 23:49:43 +0000

    Deploying to gh-pages from  @ f1d958a9709bedac0f6d418ddbcf4c7d0a38c996 🚀

--- out/common/framework/version.js
+++ out/common/framework/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'd836e71f29a606a428c6dca9a205fd61fa7a4ed8';
+export const version = 'f1d958a9709bedac0f6d418ddbcf4c7d0a38c996';
---
+++ out/webgpu/util/texture/base.js
@@ -0,0 +1,17 @@
+/**
+* AUTO-GENERATED - DO NOT EDIT. Source: https://github.com/gpuweb/cts
+**/import { standardizeExtent3D } from '../../util/unions.js';export function maxMipLevelCount({
+  size,
+  dimension = '2d' })
+
+
+
+{
+  const sizeDict = standardizeExtent3D(size);
+
+  let maxMippedDimension = sizeDict.width;
+  if (dimension !== '1d') maxMippedDimension = Math.max(maxMippedDimension, sizeDict.height);
+  if (dimension === '3d') maxMippedDimension = Math.max(maxMippedDimension, sizeDict.depth);
+  return Math.floor(Math.log2(maxMippedDimension)) + 1;
+}
+//# sourceMappingURL=base.js.map
