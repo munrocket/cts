0f5b4c3978a86c032492003f2fe75d261ea0a65d
Kangz <corentin@wallez.net>
2021-03-25 09:15:11 +0000

    Deploying to gh-pages from  @ 254299b349e7b4d18e39b89fbcba979a5caab523 🚀

--- out-wpt/common/framework/version.js
+++ out-wpt/common/framework/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '340098b7d4f735690f433daafc5585ad6d3c51be';
+export const version = '254299b349e7b4d18e39b89fbcba979a5caab523';
