225f5b6803e7f79240a52f7106983982e05364e7
Richard-Yunchao <yunchao.he@intel.com>
2021-04-13 05:50:10 +0000

    Deploying to gh-pages from  @ 711dfe0951d01f820bd66b30b4d5ea59a7096ae2 🚀

--- out-wpt/common/framework/version.js
+++ out-wpt/common/framework/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '6c9e3766c3fba0d424f32bf1f4bd7543c7ca4ecd';
+export const version = '711dfe0951d01f820bd66b30b4d5ea59a7096ae2';
