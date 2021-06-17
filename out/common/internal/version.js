dd3ab56e68e46e99f2f41833facbd7869804ec94
Kangz <corentin@wallez.net>
2021-06-17 10:02:32 +0000

    Deploying to gh-pages from @ gpuweb/cts@396a02ab134c801271921a78a5c9fb9c0c476449 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'bcc93e24ad10013efd748151a959323b45cb9590';
+export const version = '396a02ab134c801271921a78a5c9fb9c0c476449';
