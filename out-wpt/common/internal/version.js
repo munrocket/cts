8f0686346011892306d9a0bd2ad65d2131e7678e
Jiawei-Shao <jiawei.shao@intel.com>
2021-06-22 00:21:20 +0000

    Deploying to gh-pages from @ gpuweb/cts@afd8116545686089a76965a5a2f39ead32b7ed4f 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = '3d91e32844eea427011849c156520e9a11b22f5e';
+export const version = 'afd8116545686089a76965a5a2f39ead32b7ed4f';
