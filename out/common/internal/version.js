76e99199af2b89d8fb0978d22d3f9cc200db195e
kainino0x <kainino1@gmail.com>
2021-06-22 19:56:20 +0000

    Deploying to gh-pages from @ gpuweb/cts@24608e55c3ad5d7d66b59bd314a9cd70865aa94e 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'afd8116545686089a76965a5a2f39ead32b7ed4f';
+export const version = '24608e55c3ad5d7d66b59bd314a9cd70865aa94e';
---
+++ out/webgpu/util/copyToTexture.js
@@ -0,0 +1,95 @@
+/**
+* AUTO-GENERATED - DO NOT EDIT. Source: https://github.com/gpuweb/cts
+**/import { GPUTest } from '../gpu_test.js';import { align } from './math.js';
+import { kBytesPerRowAlignment } from './texture/layout.js';
+
+export class CopyToTextureUtils extends GPUTest {
+  checkCopyExternalImageResult(
+  src,
+  expected,
+  width,
+  height,
+  bytesPerPixel)
+  {
+    const exp = new Uint8Array(expected.buffer, expected.byteOffset, expected.byteLength);
+    const rowPitch = align(width * bytesPerPixel, kBytesPerRowAlignment);
+    const dst = this.createCopyForMapRead(src, 0, rowPitch * height);
+
+    this.eventualAsyncExpectation(async niceStack => {
+      await dst.mapAsync(GPUMapMode.READ);
+      const actual = new Uint8Array(dst.getMappedRange());
+      const check = this.checkBufferWithRowPitch(
+      actual,
+      exp,
+      width,
+      height,
+      rowPitch,
+      bytesPerPixel);
+
+      if (check !== undefined) {
+        niceStack.message = check;
+        this.rec.expectationFailed(niceStack);
+      }
+      dst.destroy();
+    });
+  }
+
+  checkBufferWithRowPitch(
+  actual,
+  exp,
+  width,
+  height,
+  rowPitch,
+  bytesPerPixel)
+  {
+    const bytesPerRow = width * bytesPerPixel;
+    for (let y = 0; y < height; ++y) {
+      const checkResult = this.checkBuffer(
+      actual.subarray(y * rowPitch, bytesPerRow),
+      exp.subarray(y * bytesPerRow, bytesPerRow));
+
+      if (checkResult !== undefined) return `on row ${y}: ${checkResult}`;
+    }
+    return undefined;
+  }
+
+  doTestAndCheckResult(
+  imageCopyExternalImage,
+  dstTextureCopyView,
+  copySize,
+  bytesPerPixel,
+  expectedData)
+  {
+    this.device.queue.copyExternalImageToTexture(
+    imageCopyExternalImage,
+    dstTextureCopyView,
+    copySize);
+
+
+    const externalImage = imageCopyExternalImage.source;
+    const dstTexture = dstTextureCopyView.texture;
+
+    const bytesPerRow = align(externalImage.width * bytesPerPixel, kBytesPerRowAlignment);
+    const testBuffer = this.device.createBuffer({
+      size: bytesPerRow * externalImage.height,
+      usage: GPUBufferUsage.COPY_SRC | GPUBufferUsage.COPY_DST });
+
+
+    const encoder = this.device.createCommandEncoder();
+
+    encoder.copyTextureToBuffer(
+    { texture: dstTexture, mipLevel: 0, origin: { x: 0, y: 0, z: 0 } },
+    { buffer: testBuffer, bytesPerRow },
+    { width: externalImage.width, height: externalImage.height, depthOrArrayLayers: 1 });
+
+    this.device.queue.submit([encoder.finish()]);
+
+    this.checkCopyExternalImageResult(
+    testBuffer,
+    expectedData,
+    externalImage.width,
+    externalImage.height,
+    bytesPerPixel);
+
+  }}
+//# sourceMappingURL=copyToTexture.js.map
