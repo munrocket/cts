9e57aa2b29e7849bc82526b970cd6047ca324e49
toji <tojiro@gmail.com>
2021-07-19 23:09:40 +0000

    Deploying to gh-pages from @ gpuweb/cts@f19dd8595c4cc6058ec47db576b292d072353e5e 🚀

--- out/common/internal/version.js
+++ out/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'f2825ebef39c97bb6f9547546244ebf623280bf4';
+export const version = 'f19dd8595c4cc6058ec47db576b292d072353e5e';
--- out/common/runtime/standalone.js
+++ out/common/runtime/standalone.js
@@ -37,6 +37,32 @@ const resultsVis = document.getElementById('resultsVis');
 
 
 
+function emptySubtreeResult() {
+  return { pass: 0, fail: 0, warn: 0, skip: 0, total: 0, timems: 0 };
+}
+
+function mergeSubtreeResults(...results) {
+  const target = emptySubtreeResult();
+  for (const result of results) {
+    target.pass += result.pass;
+    target.fail += result.fail;
+    target.warn += result.warn;
+    target.skip += result.skip;
+    target.total += result.total;
+    target.timems += result.timems;
+  }
+  return target;
+}
+
+
+
+
+
+
+
+
+
+
 // DOM generation
 
 function memoize(fn) {
@@ -70,10 +96,15 @@ function makeCaseHTML(t) {
   let caseResult;
 
   // Becomes set once the DOM for this case exists.
+  let clearRenderedResult;
   let updateRenderedResult;
 
   const name = t.query.toString();
   const runSubtree = async () => {
+    if (clearRenderedResult) clearRenderedResult();
+
+    const result = emptySubtreeResult();
+
     haveSomeResults = true;
     const [rec, res] = logger.record(name);
     caseResult = res;
@@ -83,7 +114,26 @@ function makeCaseHTML(t) {
       await t.run(rec);
     }
 
+    result.total++;
+    result.timems += caseResult.timems;
+    switch (caseResult.status) {
+      case 'pass':
+        result.pass++;
+        break;
+      case 'fail':
+        result.fail++;
+        break;
+      case 'skip':
+        result.skip++;
+        break;
+      case 'warn':
+        result.warn++;
+        break;}
+
+
     if (updateRenderedResult) updateRenderedResult();
+
+    return result;
   };
 
   const generateSubtreeHTML = div => {
@@ -96,6 +146,12 @@ function makeCaseHTML(t) {
     const casetime = $('<div>').addClass('testcasetime').html('ms').appendTo(casehead);
     div.appendChild(casehead);
     div.appendChild(caselogs[0]);
+
+    clearRenderedResult = () => {
+      div.removeAttribute('data-status');
+      casetime.text('ms');
+      caselogs.empty();
+    };
 
     updateRenderedResult = () => {
       if (caseResult) {
@@ -130,10 +186,22 @@ function makeCaseHTML(t) {
 }
 
 function makeSubtreeHTML(n, parentLevel) {
+  let subtreeResult = emptySubtreeResult();
+  // Becomes set once the DOM for this case exists.
+  let clearRenderedResult;
+  let updateRenderedResult;
+
   const { runSubtree, generateSubtreeHTML } = makeSubtreeChildrenHTML(
   n.children.values(),
   n.query.level);
 
+
+  const runMySubtree = async () => {
+    if (clearRenderedResult) clearRenderedResult();
+    subtreeResult = await runSubtree();
+    if (updateRenderedResult) updateRenderedResult();
+    return subtreeResult;
+  };
 
   const generateMyHTML = div => {
     const subtreeHTML = $('<div>').addClass('subtreechildren');
@@ -141,7 +209,7 @@ function makeSubtreeHTML(n, parentLevel) {
 
     // Hide subtree - it's not generated yet.
     subtreeHTML.hide();
-    const [header, setChecked] = makeTreeNodeHeaderHTML(n, runSubtree, parentLevel, checked => {
+    const [header, setChecked] = makeTreeNodeHeaderHTML(n, runMySubtree, parentLevel, checked => {
       if (checked) {
         // Make sure the subtree is generated and then show it.
         generateSubtree();
@@ -156,6 +224,23 @@ function makeSubtreeHTML(n, parentLevel) {
     div.appendChild(header);
     div.appendChild(subtreeHTML[0]);
 
+    clearRenderedResult = () => {
+      div.removeAttribute('data-status');
+    };
+
+    updateRenderedResult = () => {
+      let status = '';
+      if (subtreeResult.pass > 0) {
+        status += 'pass';
+      }
+      if (subtreeResult.fail > 0) {
+        status += 'fail';
+      }
+      div.setAttribute('data-status', status);
+    };
+
+    updateRenderedResult();
+
     return () => {
       setChecked();
       const setChildrenChecked = generateSubtree();
@@ -163,7 +248,7 @@ function makeSubtreeHTML(n, parentLevel) {
     };
   };
 
-  return { runSubtree, generateSubtreeHTML: generateMyHTML };
+  return { runSubtree: runMySubtree, generateSubtreeHTML: generateMyHTML };
 }
 
 function makeSubtreeChildrenHTML(
@@ -173,9 +258,11 @@ parentLevel)
   const childFns = Array.from(children, subtree => makeTreeNodeHTML(subtree, parentLevel));
 
   const runMySubtree = async () => {
+    const results = [];
     for (const { runSubtree } of childFns) {
-      await runSubtree();
+      results.push(await runSubtree());
     }
+    return mergeSubtreeResults(...results);
   };
   const generateMyHTML = div => {
     const setChildrenChecked = Array.from(childFns, ({ generateSubtreeHTML }) =>
@@ -238,7 +325,7 @@ onChange)
   attr('alt', runtext).
   attr('title', runtext).
   on('click', async () => {
-    await runSubtree();
+    runSubtree();
   }).
   appendTo(header);
   $('<a>').
