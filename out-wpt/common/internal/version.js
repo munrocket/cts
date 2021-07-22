f2c104e24a75533f087be2d621c6d0f741554054
jzm-intel <84498356+jzm-intel@users.noreply.github.com>
2021-07-22 01:09:09 +0000

    Deploying to gh-pages from @ gpuweb/cts@88b82ee1ac270f3a78096d75c0c82fb60b37499d 🚀

--- out-wpt/common/internal/version.js
+++ out-wpt/common/internal/version.js
@@ -1,3 +1,3 @@
 // AUTO-GENERATED - DO NOT EDIT. See tools/gen_version.
 
-export const version = 'ed998db3cf154da80a73cf797a3db54e3abdec43';
+export const version = '88b82ee1ac270f3a78096d75c0c82fb60b37499d';
